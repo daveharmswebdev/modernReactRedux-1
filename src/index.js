9146fbd7f10999f2fd9a4bcb4d4c9fae78026ea8
Dave Harms <daveharmswebdev@gmail.com>
2017-01-16 13:23:30 -0600

    added search bar

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBcOeFnk_zXPst8rEtUZax6SfQXOa860WA';
+
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 };
 
 ReactDom.render(<App />, document.querySelector('.container'));
